47da0023582f2bd6699905817087fd40f3041604
Beto <a.danielqz@gmail.com>
2019-08-23 12:58:50 -0500

    dynamic table finished

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,34 @@ var app = new Vue
 ({
     el: '#app',
     data:  {
-        message: 'Hola vue.js!'
+        list : [
+
+        ],
+        name: '',
+        note: ''
+    },
+
+    methods:
+    {
+        addnote: function()
+        {
+            stateA=false;
+            if(this.note>=12.5)
+            {
+                stateA=true;
+            }
+            if(this.name!="" && this.note!="")
+            {
+                this.list.push({name:this.name, note:this.note,state:stateA});
+                this.name="";
+                this.note="";
+
+            }
+            else
+            {
+                alert("Ingrese el nombre y promedio del estudiante")
+            }
+        }
     }
 
 
